166dc879b99bc64eb84872a74ca47719f9ece1d2
andfull17 <a.fullonip@gmail.com>
2018-10-24 18:18:00 -0300

    Creacion spider Scotiabank

---
+++ ScrapyBeneficios/ScrapyBeneficios/spiders/__init__.py
@@ -0,0 +1,30 @@
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "scotiabank"
+
+    def start_requests(self):
+        urls = [
+            'https://www.scotiabank.com.uy/Tarjetas/Promociones'
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+    
+    def parse(self, response):
+        o = open("benescotia","r+")
+        entries = []
+        categories = response.xpath('//section[@class="staticInfoTiles"]//h1/text()').extract()
+        for i in range(1,len(categories)+1):
+            stores = response.xpath('//section[@class="staticInfoTiles"][' + str(i) + ']//li/h2/text()').extract()
+            for s in range(1,len(stores)+1):
+                link_to_description = response.xpath('//section[@class="staticInfoTiles"][' + str(i) + ']//li[' + str(s) + ']//a[@class="button"]/@href').extract_first()
+                entries.append((categories[i-1], stores[s-1], link_to_description))
+                o.write(str((categories[i-1], stores[s-1], link_to_description)) + '\n')
+                
+
+        
+
+
+#response.xpath('//section[@class="staticInfoTiles"][3]//li/h2/text()')[0]
+
