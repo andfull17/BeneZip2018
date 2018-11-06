e6e7fbd2754c371f5f839fb1b6c7fbf9351f0b8b
andfull17 <a.fullonip@gmail.com>
2018-11-06 11:45:54 -0300

    Agregado OCA y BROU

---
+++ ScrapyBeneficios/ScrapyBeneficios/spiders/oca.py
@@ -0,0 +1,21 @@
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "oca"
+
+    def start_requests(self):
+        urls = [
+            'https://www.oca.com.uy/institucional/promociones.aspx'
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+    
+    def parse(self, response):
+        o = open("beneoca","r+")
+        entries = []
+        categories = response.xpath('//div[@class="item"]//div[@class="bd"]/h4/text()').extract()
+        for i in range(1,len(categories)+1):
+            description = response.xpath('//div[@class="item"][' + str(i) + ']//div[@class="bd"]/p/text()').extract()
+            entries.append((categories[i-1], description))
+            o.write(str((categories[i-1], description)) + '\n')
