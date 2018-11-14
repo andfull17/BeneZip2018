3a7717f42aff70cc2214baceea4c47630cbee606
afulloni <a.fullonip@gmail.com>
2018-11-14 20:43:22 -0300

    sidebar desplegable

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -3,24 +3,24 @@ import { NgModule } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
 import { CardsComponent } from './cards/cards.component';
 import { ElasticsearchService } from '../services/elasticsearch.service';
 import { HttpClientModule } from '@angular/common/http';
 import { PrincipalComponent } from './principal/principal.component';
+import { SidebarModule } from 'ng-sidebar';
 
 @NgModule({
   declarations: [
     AppComponent,
     PrincipalComponent,
-    SidebarComponent,
     CardsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MDBBootstrapModule.forRoot(),
-    HttpClientModule
+    HttpClientModule,
+    SidebarModule.forRoot()
   ],
   providers: [ElasticsearchService],
   bootstrap: [AppComponent]
--- Frontend/src/app/principal/principal.component.ts
+++ Frontend/src/app/principal/principal.component.ts
@@ -9,12 +9,44 @@ import { ElasticsearchService } from '../../services/elasticsearch.service';
 export class PrincipalComponent implements OnInit {
 
   results: any[] = [];
+  sidebarOpened: boolean = false;
 
   constructor(private elasticSearch: ElasticsearchService) { }
 
   ngOnInit() {
+    //* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
+    var dropdown = document.getElementsByClassName("dropdown-btn");
+    var i;
+
+    for (i = 0; i < dropdown.length; i++) {
+      dropdown[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var dropdownContent = this.nextElementSibling;
+        if (dropdownContent.style.display === "block") {
+          dropdownContent.style.display = "none";
+        } else {
+          dropdownContent.style.display = "block";
+        }
+      });
+    }
   }
 
+  toggleSidebar() {
+    if (this.sidebarOpened) {
+      document.getElementById("sidebar").style.width = "0";
+      document.getElementById("principal").style.marginLeft = "0";
+      this.sidebarOpened = false;
+    } else {
+      document.getElementById("sidebar").style.width = "250px";
+      document.getElementById("principal").style.marginLeft = "250px";
+      this.sidebarOpened = true;
+    }
+    
+  }
+
+  closeNav() {
+    
+  } 
 
   getCategoryItems(categoryName) {
 
